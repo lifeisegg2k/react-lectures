f81b6cc667f37de24ed4b1a471efd1ece7bc32f5
lifeisegg <lifeisegg@gmail.com>
2021-12-15 17:32:41 +0900

    useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,35 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+  // const iRunOnlyOnce = () => {
+  //   console.log("i run only once");
+  // };
+  // useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search For", keyword);
+    }
+  }, [keyword]);
+  
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Contine"} />
+      <input 
+        value={ keyword }
+        onChange={ onChange }
+        type="text"
+        placeholder="Search here..." />
+      <h1 className={styles.title}>{ counter }</h1>
+      <button onClick={ onClick } text={"Contine"}>Click me</button>
     </div>
   );
 }
