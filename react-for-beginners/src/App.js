696bfd337b26c8fb3132cfe118c9ef66172bf40a
lifeisegg <lifeisegg@gmail.com>
2021-12-15 17:39:16 +0900

    useEffect 2

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -7,19 +7,23 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
+  // console.log("i run all the time");
   // const iRunOnlyOnce = () => {
   //   console.log("i run only once");
   // };
   // useEffect(iRunOnlyOnce, []);
   useEffect(() => {
-    console.log("CALL THE API....");
+    console.log("I run only once.");
   }, []);
   useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("Search For", keyword);
-    }
+    console.log("I run when 'keyword' changes.");
   }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.");
+  }, [keyword, counter]);
   
   return (
     <div>
