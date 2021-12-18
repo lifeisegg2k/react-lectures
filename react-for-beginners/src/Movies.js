70c7bc70f7ca6e2155b21a0f63e3c5ac24844f12
lifeisegg <lifeisegg@gmail.com>
2021-12-18 20:15:34 +0900

    movies app 3

--- react-for-beginners/src/Movies.js
+++ react-for-beginners/src/Movies.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Movie from "./Movie";
+import Movie from "./components/Movie";
 
 function Movies() {
   const [loading, setLoading] = useState(true);
