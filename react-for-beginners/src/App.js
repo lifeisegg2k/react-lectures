0991e692b38f3e2d7727d6c6526578ebba1684fe
lifeisegg <lifeisegg@gmail.com>
2021-12-18 19:05:32 +0900

    Coin Tracker

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,13 +1,17 @@
 import { useState, useEffect } from "react";
 import Button from "./Button";
 import TodoList from "./TodoList";
+import CoinTracker from "./CoinTracker";
 
 function App() {
   
   return (
     <div>
       <Button text="test button"/>
+      <hr />
       <TodoList />
+      <hr />
+      <CoinTracker />
     </div>
   );
 }
