68db2c3c34f47582631593dbd89cd2e77c4b0786
lifeisegg <lifeisegg@gmail.com>
2021-12-18 18:25:57 +0900

    todo list 2

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,30 +1,13 @@
 import { useState, useEffect } from "react";
+import Button from "./Button";
+import TodoList from "./TodoList";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  // console.log(toDo);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos);
+  
   return (
     <div>
-      <h1>My To Dos ({ toDos.length })</h1>
-      <form onSubmit={ onSubmit }>
-        <input
-          onChange={ onChange }
-          value={ toDo }
-          type="text"
-          placeholder="Write your to do...." />
-        <button>Add To Do</button>
-      </form>
+      <Button text="test button"/>
+      <TodoList />
     </div>
   );
 }
